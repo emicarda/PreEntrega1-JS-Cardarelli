e0af32b52c5d90f2ee27358b4b7dfc8eafa28779
Emiliano Cardarelli <emi_carda@hotmail.com>
2023-04-19 21:48:46 -0300

    segunda entrega

--- script/main.js
+++ script/main.js
@@ -1,91 +1,181 @@
-// Ingreso de Usuario y Contrasena
 
 
-/*nction loginUsuario (){
-    let usuario = prompt ("Ingrese el nombre de usuario");
-    let contrasena = prompt("Ingrese contraseña");
-    if ((usuario == "" || contrasena == "")) {
-        alert("Por favor completar los campos requeridos");
-       }
-    if(usuario.length < 12){
-        alert("El usuario no puede tener mas de 12 caracteres.");
-    }
-    if(contrasena.length < 8){
-        alert("La contraseña debe contener mas de 8 caracteres");
+class Productos {
+    constructor(id,nombre,precio){
+        this.id = id; 
+        this.nombre = nombre;
+        this.precio = precio;
     }
 }
 
-loginUsuario();
-*/
+const productos = []
+
+productos.push(new Productos(1, "Chulengo", 30000))
+productos.push(new Productos(9, "Brasero", 12000))
+productos.push(new Productos(12, "Atizador", 6000))
+productos.push(new Productos(5, "Disco", 9000))
+
+console.log(productos)
+
+//MENU (POR FAVOR ENTRE A LA PAGINA Y HABRA EL INSPECTOR, RECARGUE LA PAGINA E INICIE SU BUSQUEDA)
+
+do{
+    menu = Number(prompt("Ingrese una opcion: \n 1.Mostrar Productos \n 2. Agregar Productos \n 3. Filtrar Productos (Menor precio al Mayor Precio ) \n 4. Limpiar Lista \n 5. Salir"))
 
 
-//alert("Agregar al carrito:\n 1. Chulengo con fogonero -> $30.000. \n 2. Estaca -> $17.000. \n 3. Disco de arar -> $20.000")
-
-let producto = prompt("Agregar al carrito:\n 1. Chulengo con fogonero -> $30.000. \n 2. Estaca -> $17.000. \n 3. Disco de arar -> $20.000. \n \n Ingrese que producto desea (1, 2 o 3)")
-let precio = Number
-
-
-switch (producto){
-    case "1":
-        precio = 30000
-        alert("Pagaras $" + precio)
-        break;
-
-    case "2":
-        precio = 17000
-        alert("Pagaras $" + precio)
+switch (menu) {
+    case 1:
+        mostrar();
         break;
     
-    case "3":
-        precio = 20000
-        alert("Pagaras $" + precio)
+        case 2:
+        agregar();
         break;
 
-    default:
+    case 3:
+        filtrar();
         break;
+    
+    case 4:
+        limpiar();
+        break;
+
+        case 5:
+            alert("Gracias por la compra")
+            break;
+    default:
+        alert("No ingreso ninguna opcion. Por favor Ingrese alguna de las opciones")
+        break;
+}
+} while(menu !=4)
+
+
+//Funcion Mostrar
+
+function mostrar(){
+    if(productos.length==0){
+        alert("Aun no ha cargado ningún producto a la lista de stock")
+    }else{
+    console.log("El stock cargado al sistema hasta el momento es el siguiente: \n\n");
+    productos.forEach(element => {
+        console.log(
+        "- ID del producto: " + element.id + "\n" + 
+        "- Nombre del producto: " + element.nombre + "\n" + 
+        "- Precio del producto: $" + element.precio + "\n\n");
+    });
+    }
 }
 
 
-//Formas de pago
-//Efectivo 10% desc
-// Tarjeta 1 sin interes
-// 3 cuotas con 10% de recargo
-// 6 cuotas con 20% de recargo
 
-let formaDePago = prompt ("Indique su forma de pago: (1 efectivo, 2 tarjeta)")
-let total = Number
+//Funcion Agregar
 
-let cuota1 = (n1, n2) => {return resultado = n1 / n2};
-let cuota3= (n1, n2) => {return resultado = (n1+(n1*0.1))/n2};
-let cuota6= (n1, n2) => {return resultado = (n1+(n1*0.2))/n2};
+function agregar(){
+    let nuevoIngreso = 0
+
+    while (nuevoIngreso < 1) {
+        let nuevo = new Productos(prompt("Ingrese nuevo id"),
+                                    (prompt("Ingrese nombre del producto")),
+                                    Number(prompt("Ingrese precio")))
+        productos.push(nuevo)
+        nuevoIngreso++
+    }
+    console.log(productos)
+}
 
 
-if (formaDePago =="1"){
-    total = precio - (precio * 0.1)
-    alert ("Total a pagar= " + total) 
-} else if (formaDePago =="2"){
+//Funcion Filtrar
 
-    let cuotas = prompt ("En cuantas cuotas quiere pagar? En 1, 3 o 6 cuotas")
-    let precioTotal
-    switch(cuotas){
-        case "1":
-            total = cuota1(precio, 1)
-            alert("El precio a pagar es $" + precio + ", en 1 cuota de $" + total)
+function filtrar(){
+    if(productos.length==0){
+    alert("Aun no ha cargado ningún producto a la lista de stock")
+    }else{
+        let menuOrden = Number(prompt(
+        "- Ingrese el numero, segun como desea ordenar la lista: \n \n \
+            1) Por numeros de ID. \n \
+            2) Alfabetiicamente por nombres. \n \
+            3) Por precios. \n \
+            0) Volver."
+        ));
+        switch (menuOrden) {
+            case 1:
+                productos.sort(function (a, b) {
+                    if (a.id > b.id) {
+                    return 1;
+                    }
+                    if (a.id < b.id) {
+                    return -1;
+                    }
+                    return 0;
+                });
+                console.log("Los elementos ordenados segun su ID, quedan asi: \n\n");
+                productos.forEach(element => {
+                    console.log(
+                    "- ID del producto: " + element.id + "\n" + 
+                    "- Nombre del producto: " + element.nombre + "\n" + 
+                    "- Precio del producto: $" + element.precio + "\n\n");
+                });
             break;
-
-        case "3":
-            total = cuota3(precio, 3)
-            precioTotal = precio + (precio*0.1)
-            alert ("El precio a pagar es $" + precioTotal + ", en 3 cuotas de $" + total)
+            case 2:
+                productos.sort(function (a, b) {
+                    if (a.nombre > b.nombre) {
+                    return 1;
+                    }
+                    if (a.nombre < b.nombre) {
+                    return -1;
+                    }
+                    return 0;
+                });
+                console.log("Los elementos ordenados segun su nombre, quedan asi: \n\n");
+                productos.forEach(element => {
+                    console.log(
+                    "- ID del producto: " + element.id + "\n" + 
+                    "- Nombre del producto: " + element.nombre + "\n" + 
+                    "- Precio del producto: $" + element.precio + "\n\n");
+                });
             break;
-
-        case "6":
-            total = cuota6(precio, 6)
-            precioTotal = precio + (precio*0.1)
-            alert ("El precio a pagar es $" + precioTotal + ", en 6 cuotas de $" + total)
+            case 3:
+                productos.sort(function (a, b) {
+                    if (a.precio > b.precio) {
+                    return 1;
+                    }
+                    if (a.precio < b.precio) {
+                    return -1;
+                    }
+                    return 0;
+                });
+                console.log("Los elementos ordenados segun su precio, quedan asi: \n\n");
+                productos.forEach(element => {
+                    console.log(
+                    "- ID del producto: " + element.id + "\n" + 
+                    "- Nombre del producto: " + element.nombre + "\n" + 
+                    "- Precio del producto: $" + element.precio + "\n\n");
+                });
             break;
+            case 0:
+            break;
+            default:
+                console.log("\n No ingresó ninguno de los numeros de la lista. Ingrese nuevamente por favor \n");
+            break;
+        }
+}
+}
 
-    default:
-        break;
+
+//Funcion Limpiar
+
+function limpiar() {
+    if(productos.length==0){
+        alert("Aun no ha cargado ningún producto a la lista de stock")
+    }else{
+    let opcion = confirm("Si confirma, borrará todo lo que ha ingresado");
+    
+    if (opcion) {
+        productos.splice(0,productos.length)
+        console.clear();
+        alert("\n\n Borró todo, debe volver a empezar :) \n\n");
+    } else {
+        alert("\n\n No se borró nada, puede seguir ingresando :) \n\n");
+    }
     }
 }
